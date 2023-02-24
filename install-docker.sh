3f5dd3362e8ff2db4c511026d71fe6bfad707379
eternal-flame <blackgeminid@gmail.com>
2023-02-24 13:21:25 +0700

    Add install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+# Uninstall old version
+echo "[+] Uninstalling old docker version"
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+# Setup Repo
+echo "[+] Setting up docker repo"
+sudo apt-get update
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+# Install docker gpg key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+# Install docker engine
+echo "[+] Installing docker engine"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Add current user to docker group
+echo "[+] Add current user to docker group"
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+# Install docker-compose
+echo "[+] Installing docker-compose"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+echo "[+] DONE"
+su - $USER
